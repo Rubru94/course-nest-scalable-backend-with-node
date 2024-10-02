4e1d473b68ab8acb30596642bdb9cff353c3c792
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-10-02 11:36:36 +0200

    feat(04-teslo-shop): Complete entity Product

--- nest/04-teslo-shop/src/products/entities/product.entity.ts
+++ nest/04-teslo-shop/src/products/entities/product.entity.ts
@@ -7,4 +7,22 @@ export class Product {
 
   @Column('text', { unique: true })
   title: string;
+
+  @Column('numeric', { default: 0 })
+  price: number;
+
+  @Column({ type: 'text', nullable: true }) // different ways to use @Column() decorator
+  description: string;
+
+  @Column('text', { unique: true })
+  slug: string;
+
+  @Column('int', { default: 0 })
+  stock: number;
+
+  @Column('text', { array: true }) // array --> supported only by postgres --> We can afford not to use another table for sizes because it has been established that it is not null, i.e. there will never be a product without sizes. Otherwise, according to database normalization rules we should create another table for the relationship.
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
 }
