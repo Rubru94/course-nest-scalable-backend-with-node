0d8e3d9f80d7ef08e22d9dbd74fb3431e2964c34
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-10-03 12:05:41 +0200

    feat(04-teslo-shop): Find using query builder

--- nest/04-teslo-shop/src/products/products.service.ts
+++ nest/04-teslo-shop/src/products/products.service.ts
@@ -7,11 +7,11 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
-import { FindOptionsWhere, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
+import { validate as isUUID } from 'uuid'; // it can be used method from class-validator too --> import { isUUID } from 'class-validator';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
-import { validate as isUUID } from 'uuid'; // it can be used method from class-validator too --> import { isUUID } from 'class-validator';
 @Injectable()
 export class ProductsService {
   private readonly logger = new Logger('ProductsService');
@@ -38,12 +38,26 @@ export class ProductsService {
   }
 
   async findOne(term: string): Promise<Product> {
-    let findOptions: FindOptionsWhere<Product> = { slug: term };
-    if (isUUID(term)) findOptions = { id: term };
+    let product: Product;
+    if (isUUID(term))
+      product = await this.productRepository.findOneBy({ id: term });
+    else {
+      const qb = this.productRepository.createQueryBuilder();
+      product = await qb
+        .where(
+          'UPPER(title)=:title or slug=:slug', // slug is always in lowercase & exact match is desired
+          {
+            title: term.toUpperCase(),
+            slug: term, // if an exact match is not desired --> term.toLowerCase(),
+          },
+        )
+        .getOne();
+    }
 
-    const product = await this.productRepository.findOneBy(findOptions);
     if (!product)
-      throw new NotFoundException(`Not found product with id: ${term}`);
+      throw new NotFoundException(
+        `Not found product with term(id, title or slug): ${term}`,
+      );
     return product;
   }
 
