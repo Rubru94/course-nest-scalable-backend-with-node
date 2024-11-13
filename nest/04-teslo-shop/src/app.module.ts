7ec062b6ef7f5e41929e3c6c3519c3593fa7b89c
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-11-13 13:38:27 +0100

    feat(04-teslo-shop): Serve static images from folder as public

--- nest/04-teslo-shop/src/app.module.ts
+++ nest/04-teslo-shop/src/app.module.ts
@@ -5,6 +5,8 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { join } from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
   imports: [
@@ -19,6 +21,8 @@ import { FilesModule } from './files/files.module';
       autoLoadEntities: true,
       synchronize: Boolean(process.env.DB_SYNCHRONIZE), // It shouldn't be used in production - otherwise you can lose production data.
     }),
+    // It is possible to retrieve the images on the url: http://localhost:3000/products/[image-name]
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
     CommonModule,
     ProductsModule,
     SeedModule,
