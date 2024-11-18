5f2516870d49c27048b35c21b9abd9b38bba7bb2
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-11-18 15:59:21 +0100

    feat(04-teslo-shop): Handle JWT strategy

---
+++ nest/04-teslo-shop/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,47 @@
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  /**
+   * By default JwtStrategy injectable class used as provider in the module will already
+   * check that the JWT is not expired and that the JWT_SECRET is valid.
+   *
+   * But we also include an additional validation. --> user.isActive
+   */
+  async validate(payload: JwtPayload): Promise<User> {
+    const { email } = payload;
+
+    const user = await this.userRepository.findOneBy({ email });
+
+    if (!user)
+      throw new NotFoundException(
+        `Not found user with email: ${email}. Token not valid.`,
+      );
+
+    if (!user.isActive) throw new UnauthorizedException('User is inactive');
+
+    return user;
+  }
+}
