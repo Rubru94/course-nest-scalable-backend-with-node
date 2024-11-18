c1dd3262c0fc6ccd94de2459b2c125bc229e91c5
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-11-18 09:28:29 +0100

    feat(04-teslo-shop): Create user

--- nest/04-teslo-shop/src/auth/auth.service.ts
+++ nest/04-teslo-shop/src/auth/auth.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  private readonly logger = new Logger('UsersService');
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create(createUserDto);
+      await this.userRepository.save(user);
+      return user;
+    } catch (error) {
+      this.handleDBException(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  // type return as never because this function always throw an error
+  private handleDBException = (error: any): never => {
+    const POSTGRESQL_UNIQUE_VIOLATION_ERROR = '23505';
+    const { code, detail } = error;
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+    if (code === POSTGRESQL_UNIQUE_VIOLATION_ERROR)
+      throw new BadRequestException(detail);
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+    this.logger.error(error, { ...error }); // print error as log & error object detailed
+    throw new InternalServerErrorException(
+      'Unexpected error, check server logs.',
+    );
+  };
 }
