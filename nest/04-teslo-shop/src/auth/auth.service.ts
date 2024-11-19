a5440dc43c7766a4074229e59cfd562ade4416a7
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-11-19 12:49:37 +0100

    feat(04-teslo-shop): Generate JWT to authenticate

--- nest/04-teslo-shop/src/auth/auth.service.ts
+++ nest/04-teslo-shop/src/auth/auth.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { handleDBException } from '../common/handlers/error.handler';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+
+type AuthenticatedUser = Partial<User> & { token: string };
 
 @Injectable()
 export class AuthService {
@@ -13,9 +17,10 @@ export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
   ) {}
 
-  async createUser(createUserDto: CreateUserDto) {
+  async createUser(createUserDto: CreateUserDto): Promise<AuthenticatedUser> {
     try {
       const saltRounds = 10;
       const { password, ...userData } = createUserDto;
@@ -27,14 +32,13 @@ export class AuthService {
       await this.userRepository.save(user);
       delete user.password;
 
-      return user;
-      // TODO: Return access JWT
+      return { ...user, token: this.getJwtToken({ email: user.email }) };
     } catch (error) {
       handleDBException(error, this.logger);
     }
   }
 
-  async login(loginUserDto: LoginUserDto) {
+  async login(loginUserDto: LoginUserDto): Promise<AuthenticatedUser> {
     const { email, password } = loginUserDto;
 
     /**
@@ -54,6 +58,11 @@ export class AuthService {
     if (!bcrypt.compareSync(password, user.password))
       throw new UnauthorizedException('Credentials are not valid (password)');
 
-    return user;
+    return { ...user, token: this.getJwtToken({ email: user.email }) };
+  }
+
+  private getJwtToken(payload: JwtPayload) {
+    const token = this.jwtService.sign(payload);
+    return token;
   }
 }
