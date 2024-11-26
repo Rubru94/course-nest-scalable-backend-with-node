a5ae5621492c726689e1dd8c20ccaea8ea54ab24
Ruben Rubio Perucha <ruben.rubioperucha@plexus.es>
2024-11-26 09:58:54 +0100

    feat(04-teslo-shop): Handling connections and disconnections

--- nest/04-teslo-shop/src/messages-ws/messages-ws.gateway.ts
+++ nest/04-teslo-shop/src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,27 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
+/**
+ *  @info Websocket concepts
+ *
+ *  - Namespace: is a communication channel that allows you to split the logic of your application over a single shared connection. If not specified points to root ('/').
+ */
+@WebSocketGateway({ cors: true /* , namespace: '/' */ })
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messagesWsService: MessagesWsService) {}
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`Client connected: ${client.id}`, ...args);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
 }
